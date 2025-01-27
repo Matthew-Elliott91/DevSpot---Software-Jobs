3b9b9617143aead1c9c0b8ec1d4fccfecf22728e
Matthew Elliott <m.elliott091091@gmail.com>
2025-01-27 17:00:44 +0000

    Added TinyMCE for a WUSIWYG Text editor, created the job postings controller with a simple index and create method. Created the form for create job postings

---
+++ DevSpot/Controllers/JobPostingsController.cs
@@ -0,0 +1,37 @@
+﻿using System.Security.AccessControl;
+using DevSpot.Models;
+using DevSpot.Repositories;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace DevSpot.Controllers
+{
+    public class JobPostingsController : Controller
+    {
+        private readonly IRepository<JobPosting> _jobPostingRepository;
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public JobPostingsController(UserManager<IdentityUser> userManager, IRepository<JobPosting> repository)
+        {
+            _jobPostingRepository = repository;
+            _userManager = userManager;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var jobPostings = await _jobPostingRepository.GetAllAsync();
+            return View(jobPostings);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(JobPosting jobPosting)
+        {
+           return RedirectToAction(nameof(Index));
+        }
+    }
+}
